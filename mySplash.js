cb9bf379c12a078e8ec6d523d21d5f0414399c6b
JGAINE16 <168437131+JGAINE16@users.noreply.github.com>
2024-04-29 10:16:54 -0400

    Add files via upload

---
+++ mySplash.js
@@ -0,0 +1,49 @@
+class Splash {
+
+ constructor() {
+   
+  this.splashBorder = 100;
+  fill(255);
+  stroke(255, 0, 0)
+  rect(this.splashBorder, this.splashBorder, windowWidth-this.splashBorder*2, windowHeight-this.splashBorder*2);
+  // draw a rectangle like this in a 3D enviornment
+  // rect(this.splashBorder-(windowWidth/2), this.splashBorder-(windowHeight/2), windowWidth-this.splashBorder*2, windowHeight-this.splashBorder*2);
+  fill(0, 0, 222);
+  strokeWeight(3)
+   
+  line(windowWidth-this.splashBorder-40, this.splashBorder+20,windowWidth-this.splashBorder-20, this.splashBorder+40)
+   line(windowWidth-this.splashBorder-20, this.splashBorder+20,windowWidth-this.splashBorder-40, this.splashBorder+40)
+   
+  this.title = createDiv("Audio Images");
+  this.title.style('color:deeppink');
+  this.title.style('font-family: Arial, Helvetica, sans-serif');
+  this.title.position(this.splashBorder+20, this.splashBorder+20);
+  
+  this.name = createDiv("Jordan Gaines");
+  this.name.position(this.splashBorder+20, this.splashBorder+60);
+  
+  this.info = createDiv("In my p5.js project, I've created an interactive experience that combines visuals and sound. Using microphone input, I detect pitch which affects both the color and size of shapes drawn on the canvas. With a mouse click, I can toggle between drawing shapes and change the background color by pressing keys '1' or '2'. Additionally, there's a mode switch triggered by mouse press, where shape color is determined by a musical scale.<p> I hope you Enjoy. <p> <a href=https://editor.p5js.org/bcjacobs/sketches/IhHSvjyZJ>view code</a>");
+  
+  this.info.position(this.splashBorder+20, this.splashBorder+100);
+  this.info.size(windowWidth-this.splashBorder*2-50, windowHeight-this.splashBorder*2-50)
+   
+
+  
+}
+  
+  update(){
+       if(mouseX > windowWidth-this.splashBorder-40 && 
+          mouseX < windowWidth-this.splashBorder-20 
+          && mouseY < this.splashBorder+40 
+          && mouseY > this.splashBorder+20
+     ){
+     return true
+   }
+  }
+ 
+  hide(){
+    this.title.remove()
+    this.name.remove()
+    this.info.remove()
+  }
+}
