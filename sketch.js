dfdbdd2d87e9f70f2abed0b058a4660c447ec42f
JGAINE16 <168437131+JGAINE16@users.noreply.github.com>
2024-04-30 09:22:07 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,98 @@
+var mode = 0;
+var audioStarted = false;
+let vol = 0.0;
+let mic;
+let pitch;
+let audioContext;
+let colors = [];
+let shape = false;
+let bgColor = 255; // Initial background color
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  audioContext = getAudioContext().suspend();
+  mic = new p5.AudioIn();
+  mic.start(startPitch); // Start pitch detection after mic starts
+  splash = new Splash ();
+}
+
+function draw() {
+  if (mouseIsPressed == true && splash. update()==true) {
+    mode = 1;
+    background(255)
+  }
+  if (mode == 1) {
+    splash.hide();
+    
+    
+    // Change the colorMode to HSB
+    colorMode(HSB); // 360, 100, 100, 1.0
+   // Define the color palette
+    for (let i = 0; i < scale.length; i++) {
+      let newColor = color(i * 360 / scale.length, 50, 100, 0.9);
+      colors.push(newColor);
+    }
+    stroke(0, 0, 0, 0.5);
+  
+  }
+
+  // Use the volume from the microphone to control the size
+  vol = mic.getLevel();
+  let diameter = vol * 800; // Larger starting size
+
+  let r = random(0, 255);
+  let b = random(0, 255);
+  let g = random(0, 255);
+  fill(r, b, g);
+
+  if (shape === false && mode ==1) {
+    ellipse(mouseX, mouseY, diameter);
+  } else {
+    rect(mouseX - diameter / 2, mouseY - diameter / 2, diameter, diameter);
+  }
+}
+
+// Function to start pitch detection
+function startPitch() {
+  pitch = ml5.pitchDetection(model_url, audioContext, mic.stream, modelLoaded);
+}
+
+// Function to handle model loading
+function modelLoaded() {
+  select('#status').html('Model Loaded');
+  getPitch();
+}
+
+// Function to get the pitch, find the closest note, and set the fill color
+function getPitch() {
+  pitch.getPitch(function (err, frequency) {
+    if (frequency) {
+      let midiNum = freqToMidi(frequency);
+      currentNote = scale[midiNum % 12];
+      fill(colors[midiNum % 12]);
+      select('#noteAndVolume').html('Note: ' + currentNote + " - volume " + nf(vol, 1, 2));
+    }
+    getPitch(); // Call getPitch recursively
+  });
+}
+
+// Toggle shape on mouse press
+function mousePressed() {
+  if (!audioStarted) {
+    userStartAudio();
+    audioStarted = true;
+    mode = 1
+  }
+  shape = !shape;
+}
+
+// Change background color on key press
+function keyPressed() {
+  if (key === '1') {
+    bgColor = color('rgb(240,217,221)'); // Change background to pink
+  } else if (key === '2') {
+    bgColor = color('rgb(219,241,219)'); // Change background to turquoise
+  }
+  background(bgColor);
+}
